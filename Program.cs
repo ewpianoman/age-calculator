29fb8dc6313d91f55271f2b29461dd1c925b5b83
Eric Wilson <ewpiano@Erics-MacBook-Pro.local>
2021-10-17 15:41:07 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace age_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Greet user
+
+            // 2. Ask user for name
+
+            // 3. Ask user for age
+
+            // 4. Calculate age in months, days, minutes, and seconds
+        }
+    }
+}
